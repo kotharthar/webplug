79a5106c7d426ccaab125a76433416a7f6d31c77
Thar Htet <kotharthar@gmail.com>
2016-01-26 17:31:12 +0630

    HTTP client is implemented to retrieve file list from server.

--- site_app/app/scripts/controllers/main.js
+++ site_app/app/scripts/controllers/main.js
@@ -42,21 +42,22 @@ angular.module('webplugApp')
             labelSelected: "a8"
         }
     }
-    $scope.dataForTheTree =
-        [
-        { "name" : "Joe", "age" : "21", "children" : [
-            { "name" : "Smith", "age" : "42", "children" : [] },
-            { "name" : "Gary", "age" : "21", "children" : [
-                { "name" : "Jenifer", "age" : "23", "children" : [
-                    { "name" : "Dani", "age" : "32", "children" : [] },
-                    { "name" : "Max", "age" : "34", "children" : [] }
-                    ]}
-                ]}
-            ]},
-        { "name" : "Albert", "age" : "33", "children" : [] },
-        { "name" : "Ron", "age" : "29", "children" : [] }
-    ];
+    // $scope.dataForTheTree =
+    //     [
+    //     { "name" : "Joe", "age" : "21", "children" : [
+    //         { "name" : "Smith", "age" : "42", "children" : [] },
+    //         { "name" : "Gary", "age" : "21", "children" : [
+    //             { "name" : "Jenifer", "age" : "23", "children" : [
+    //                 { "name" : "Dani", "age" : "32", "children" : [] },
+    //                 { "name" : "Max", "age" : "34", "children" : [] }
+    //                 ]}
+    //             ]}
+    //         ]},
+    //     { "name" : "Albert", "age" : "33", "children" : [] },
+    //     { "name" : "Ron", "age" : "29", "children" : [] }
+    // ];
     $scope.showSelected = function(sel) {
          $scope.selectedNode = sel;
+         console.log($scope.selectedNode);
      };
 }]);
